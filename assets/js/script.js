4934a3e03252a2397212694234ba0cb39494ea1c
Retr01234 <>
2023-06-14 22:12:47 +0000

    feature: add wining combinations

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ let mainGame = document.getElementById("play-area");
 let cellBlocks = document.querySelectorAll(".board-cells");
 let scoreboard = document.getElementById("scoreboard-info");
 
+/* 1st Player */
 let playerOne = "X";
+
+/* Providing Multiple Winning Scores */
+let waysOfWinning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 /* Converting Cell Blocks to an Array to make Looping through it easier */
 cellBlocks = Array.from(cellBlocks);
